14063b4d104ba01b8a448806a940de618ebe23aa
Austin <atrain742@gmail.com>
2018-04-06 22:52:47 -0400

    Init commit

---
+++ wavFormatter/Header.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wavFormatter
+{
+    // Source of .WAV file specification: http://soundfile.sapp.org/doc/WaveFormat/
+
+    class Header
+    {
+        // 0x52494646 big-endian ASCII
+        private char[] chunkID = new char[4]; // "RIFF" Chunk ID
+        private char[] chunkSize = new char[4];
+        private char[] format = new char[4]; // "WAVE"
+        private char[] subchunkID = new char[4];
+
+
+    }
+}
